64ae183be8947adb0e228fe064adadf9f502842e
hoontee <5272529+hoontee@users.noreply.github.com>
2020-07-02 18:49:10 -0500

    Update ExtendGizmoPlugin.gd

--- addons/extend/ExtendGizmoPlugin.gd
+++ addons/extend/ExtendGizmoPlugin.gd
@@ -1,3 +1,26 @@
+#************************************************************************#
+#  ExtendGizmoPlugin.gd                                                  #
+#************************************************************************#
+#                        This file is part of:                           #
+#                               EXTEND                                   #
+#                https://github.com/hoontee/godot-extend                 #
+#************************************************************************#
+#  Copyright (c) 2020 hoontee @ Iron Stag Games.                         #
+#                                                                        #
+#  Extend is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by  #
+#  the Free Software Foundation, either version 3 of the License, or     #
+#  (at your option) any later version.                                   #
+#                                                                        #
+#  Extend is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+#                                                                        #
+#  You should have received a copy of the GNU General Public License     #
+#  along with Extend.  If not, see <https://www.gnu.org/licenses/>.      #
+#************************************************************************#
+
 extends EditorSpatialGizmoPlugin
 
 const Gizmo := preload("res://addons/extend/ExtendGizmo.gd")
@@ -20,3 +43,4 @@ func create_gizmo(spatial: Spatial) -> EditorSpatialGizmo:
 
 func get_name() -> String:
 	return "﻿Extend:" + gizmo_class_name
+#           ^-- This is a ZERO WIDTH NO-BREAK SPACE (U+FEFF)
