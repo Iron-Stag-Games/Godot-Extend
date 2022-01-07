343d7c3e3fd9e9203e81fb034f2b945793138767
hoontee <hunter.chambers@gmail.com>
2022-01-06 23:15:54 -0600

    Switch license from AGPL to LGPL

--- addons/extend/ExtendGizmo.gd
+++ addons/extend/ExtendGizmo.gd
@@ -3,22 +3,22 @@
 #*******************************************************************************#
 #                             This file is part of:                             #
 #                                    EXTEND                                     #
-#                    https://github.com/hoontee/godot-extend                    #
+#                https://github.com/Iron-Stag-Games/Godot-Extend                #
 #*******************************************************************************#
 #  Copyright (c) 2020 hoontee @ Iron Stag Games.                                #
 #                                                                               #
-#  Extend is free software: you can redistribute it and/or modify               #
-#  it under the terms of the GNU Affero General Public License as published by  #
-#  the Free Software Foundation, either version 3 of the License, or            #
-#  (at your option) any later version.                                          #
+#  This library is free software; you can redistribute it and/or                #
+#  modify it under the terms of the GNU Lesser General Public                   #
+#  License as published by the Free Software Foundation; either                 #
+#  version 3 of the License, or (at your option) any later version.             #
 #                                                                               #
-#  Extend is distributed in the hope that it will be useful,                    #
+#  This library is distributed in the hope that it will be useful,              #
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of               #
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
-#  GNU Affero General Public License for more details.                          #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU            #
+#  Lesser General Public License for more details.                              #
 #                                                                               #
-#  You should have received a copy of the GNU Affero General Public License     #
-#  along with Extend.  If not, see <https://www.gnu.org/licenses/>.             #
+#  You should have received a copy of the GNU Lesser General Public License     #
+#  along with this library; if not, see <https://www.gnu.org/licenses/>.        #
 #*******************************************************************************#
 
 extends EditorSpatialGizmo
